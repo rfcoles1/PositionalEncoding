d686a72eafb9145388db0f3cf9c1b3d02acd5371
RColes <rfcoles@hotmail.co.uk>
2020-09-20 20:17:48 -0700

    added CoordConv

---
+++ CoordConv/CoordConv.py
@@ -0,0 +1,74 @@
+from Networks import *
+
+sys.path.insert(1, '../Utils')
+from metrics import *
+from datasets import *
+
+
+def addCoordLayers(x):
+    new_dims = np.append(np.shape(x)[:-1],2)
+    new = np.zeros(new_dims)
+
+    dim1 = np.shape(x)[1]
+    for i in range(dim1):
+        new[:,i,:,0] = i/dim1
+    dim2 = np.shape(x)[2]
+    for j in range(dim2):
+        new[:,:,j,1] = j/dim2
+
+    return np.concatenate([x,new], axis=-1)
+
+class CoordNet(Network):    
+    def __init__(self, input_shape):
+        super().__init__()
+
+        self.dirpath = 'records_z/'
+        if not os.path.exists(self.dirpath):
+            os.makedirs(self.dirpath)
+
+        self.batch_size = 64
+        self.input_shape = input_shape
+        self.num_out = 1
+        self.lr = 0.0001
+
+        optimizer = keras.optimizers.Adam(lr=self.lr)
+
+        self.inp = layers.Input(input_shape)
+        self.Net = tf.keras.models.Model(self.inp, self.tf_resnet(self.inp))
+        self.Net.compile(loss=tf.keras.losses.MSE,\
+            optimizer=optimizer)
+        self.es = tf.keras.callbacks.EarlyStopping(monitor='loss',\
+            patience=10, verbose=2, restore_best_weights=True)
+
+    def tf_resnet(self,x):
+        base_model = tf.keras.applications.ResNet50(include_top=False, weights=None,\
+            input_shape=self.input_shape)
+        base_model.trainable = True
+        x = base_model(x, training=True)
+        x = layers.GlabelAveragePooling2D()(x)
+
+        x = layers.Dense(256,activation=layers.LeakyReLU(alpha=0.1))(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(128,activation=layers.LeakyReLU(alpha=0.1))(x)
+        x = layers.Dropout(0.5)(x)
+        x = layers.Dense(1)(x)
+        return x
+
+    def train(self, x_train, y_train, x_test, y_test, epochs, verbose=2):
+        batch_hist = LossHistory()
+
+        History = self.Net.fit(x_train, y_train,\
+            batch_size=self.batch_size,
+            epochs=epochs,
+            verbose=verbose,
+            validation_data=(x_test, y_test),
+            callbacks=[batch_hist, self.es])
+
+        epochs_arr = np.arange(self.curr_epoch)
+        iterations = np.ceil(np.shape(x_train)[0]/self.batch_size)
+
+        self.hist['epochs'].append(epochs_arr)
+        self.hist['iterations'].append(epochs_arr*iterations)
+        self.hist['train_MSE'].append(History.history['loss'])
+        self.hist['batch_MSE'].append(batch_hist.history['loss'])
+        self.hist['test_MSE'].append(History.history['val_loss'])
---
+++ Utils/plot_results.py
@@ -0,0 +1,125 @@
+import numpy as np
+import pickle 
+import os
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+import scipy.stats as stats
+from scipy.signal import savgol_filter
+
+big = 32
+med = 24
+smol = 18
+
+plt.rc('font', size=smol)
+plt.rc('figure', titlesize=big)
+plt.rc('legend', fontsize=smol)
+plt.rc('axes', titlesize=med)
+plt.rc('axes', labelsize=smol)
+plt.rc('xtick', labelsize=smol)
+plt.rc('ytick', labelsize=smol)
+
+
+def load_folder(path):
+    filelist = os.listdir(path) 
+    filelist = [f for f in filelist if '.pickle' in f]
+    histories = []
+    labels = []
+    
+    for thisfile in filelist:
+        f = open(path + '/' + thisfile, 'rb')
+        hist = pickle.load(f)
+        histories.append(hist)
+        labels.append(thisfile[:-7])
+        f.close()
+
+    return histories, labels
+
+def show_aug(img, Augmenter, channel=0, scale=False):
+       
+    aug = Augmenter.transform(img)
+
+    fig = plt.figure(figsize=(6,3))
+    grid = gs.GridSpec(1,2)
+    ax1 = fig.add_subplot(grid[0])
+    ax2 = fig.add_subplot(grid[1])
+
+
+    min1 = np.min(img[:,:,channel])
+    max1 = np.max(img[:,:,channel])
+    min2 = np.min(aug[:,:,channel])
+    max2 = np.max(aug[:,:,channel])
+    mini = min(min1, min2)
+    maxi = max(max1, max2)
+    if scale:
+        ax1.imshow(img[:,:,channel], vmin=min1, vmax=max2)
+        ax2.imshow(aug[:,:,channel], vmin=min1, vmax=max2)
+    else:
+        ax1.imshow(img[:,:,channel])
+        ax2.imshow(aug[:,:,channel])
+ 
+    ax1.axis('off')
+    ax2.axis('off')
+    plt.show()
+        
+
+def plot_resid(true, pred, y_min, y_max, xlim=[0.4,3.6], ylim=[-4,4]):
+
+    true = true*(y_max - y_min) + y_min
+    pred = pred*(y_max - y_min) + y_min
+
+    resid = true - pred
+    bias = np.median(resid)
+    std = np.std(resid)
+
+    fig = plt.figure(figsize=(8,6))
+    grid = gs.GridSpec(1,2, width_ratios=[4,1])
+
+    ax0 = fig.add_subplot(grid[0,0])
+    ax0.scatter(true, resid, color='crimson', alpha=0.5)
+    ax0.plot(xlim, [0,0], color='dimgrey', linestyle='--')
+
+    ax0.set_xlim(xlim[0], xlim[1])
+    ax0.set_ylim(ylim[0], ylim[1])
+    ax0.set_xlabel('Test Value')
+    ax0.set_ylabel('Residual')
+    ax0.grid()
+
+    n, bin_edges = np.histogram(resid, 50)
+    probs = n/np.shape(resid)[0]
+    bin_mid = (bin_edges[1:]+bin_edges[:-1])/2.0
+    bin_wid = bin_edges[1]-bin_edges[0]
+    (mu, sigma) = stats.norm.fit(resid)
+    y = stats.norm.pdf(bin_mid, mu, sigma)*bin_wid
+
+    ax1 = fig.add_subplot(grid[0,1])
+    ax1.plot(y, bin_mid, color='darkred', lw=2)
+    ax1.plot([0,max(y)], [0,0], color='dimgrey', linestyle='--')
+
+    ax1.set_xlim(0,max(y))
+    ax1.set_ylim(ylim[0],ylim[1])
+    ax1.get_xaxis().set_visible(False)
+    ax1.grid(axis='y')
+    ax1.yaxis.tick_right()
+    ax1.yaxis.set_label_position('right')
+
+def plot_diff(true, pred, y_min, y_max, xlim=[0.4,3.6]):
+
+    true = true*(y_max - y_min) + y_min
+    pred = pred*(y_max - y_min) + y_min
+
+    fig = plt.figure(figsize=(8,8))
+    grid = gs.GridSpec(1,1)
+
+    ax0 = fig.add_subplot(grid[0,0])
+    ax0.scatter(true, pred, color='crimson', alpha=0.5)
+    ax0.plot(xlim, xlim, color='dimgrey', linestyle='--')
+
+    ax0.set_xlim(xlim[0], xlim[1])
+    ax0.set_ylim(xlim[0], xlim[1])
+    ax0.set_xticks([1,2,3])
+    ax0.set_yticks([1,2,3])
+    ax0.set_xlabel('Test Value')
+    ax0.set_ylabel('Predicted Value')
+    ax0.grid()
+
+
